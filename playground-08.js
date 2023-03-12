ce001290abe8cda32d4e3d554efb13a0d76f889f
Alejandro Naranjo <alejonaranjo83@gmail.com>
2023-03-12 14:42:21 -0500

    creating playground #9 - class 26

--- playground-08.js
+++ playground-08.js
@@ -60,3 +60,10 @@ findLargestPalindrome(["racecar", "rearaer", "wearioiraew", "level", "world", "h
 findLargestPalindrome(["racecar", "level", "world", "hello"])
 
 findLargestPalindrome(["Platzi", "javascript", "html", "css"])
+
+
+const arr = [1, 2, 3, 4, 5]; 
+
+const result = arr.map(num => num * 2).filter(num => num > 5); 
+
+console.log(result)
