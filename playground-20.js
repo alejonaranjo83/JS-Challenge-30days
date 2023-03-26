6fd875ca917b65ce47b95bc2ec9fca8662f2b639
Alejandro Naranjo <alejonaranjo83@gmail.com>
2023-03-26 14:09:56 -0500

    playground #20 solved. This was the third checkpoint of the challenge

--- playground-20.js
+++ playground-20.js
@@ -116,63 +116,109 @@
 
 
 
-function hotelSystem(rooms) {
-    const reservations = [];
+function hotelSystem(rooms) {//a function that recives the amount of rooms to work with. Inside that function, there are some methods to manage hotel reservations
+    const reservations = []; //array that will receive reservations created with other method
     
-    return {
-        searchReservation(id) {
-            const a = reservations.find(obj => obj.id === id);
-            
-            if(!a) {
-                throw new Error("La reservación no fue encontrada");
-            } else {
-                console.log(a);
-                return a;
-            };
-        },
 
-        getSortReservations() {
-            const reservationsSorted = reservations.map((reservation) => ({
-                ...reservation,
-                checkIn: new Date(reservation.checkIn),
-            }))
-            .sort((a, b) => a.checkIn - b.checkIn);
-            console.log(reservationsSorted);
-        },
+    function addReservation(reservation) {//add a reservation to the array if the room es available for the desired dates.
+        // make sure that room is available for the check-in and check-out dates
+        const availability = reservations.find((r) => {
+            return (
+                r.roomNumber === reservation.roomNumber &&
+                ((r.checkIn <= reservation.checkIn &&
+                    r.checkOut > reservation.checkIn) ||
+                    (r.checkIn < reservation.checkOut && r.checkOut >= reservation.checkOut))
+            )
+        })
 
-        addReservation(reservation) {
-            // make sure that room is available for the check-in and check-out dates
+        if(availability) {
+            throw new Error("La habitación no está disponible");
+        } else {
             reservations.push(reservation);
+            return "Reserva agregada exitosamente";
+        }
+    }
 
-            // if (condition) {
-            //     reservations.push(reservation);
-            // } else {
-            //     throw new Error("La habitación no está disponible");
-            // }
-        },
+    function removeReservation(id) {//delete a reservation according to the specified ID
+        const index = reservations.findIndex(obj => obj.id === id);//search the index of the reservation that wants to be deleted. If an index is found, the method receives the index; in the contrary, returns -1
+        
+        if(index > -1) { //if the index is different than -1... the reservation exists...
+            const removedReservation = reservations[index];
+            reservations.splice(index, 1); //delete the element in the specified position
+            return removedReservation; //return the deleted reservation
+        } else {//if there´s no index, then throw an error
+            throw new Error("La reservación que se busca remover no existe");
+        };
+    }
 
-        removeReservation(id) {
-            const index = reservations.findIndex(obj => obj.id === id);
+    function searchReservation(id) {//search for a particular according to their ID
+        const r = reservations.find(obj => obj.id === id);//search inside the array, the object that contains the reservation.
+        
+        if(!r) {//if there´s no reservation...
+            throw new Error("La reservación no fue encontrada");
+        } else {//if there´s a reservation...
+            // console.log(r);
+            return r;
+        };
+    }
+
+    function getReservations() { //return the reservations that have been made
+        // console.log(reservations);
+        return reservations;
+    }
+
+    function getSortReservations() {//return reservations according to the checking order (ascending order)
+        const sortedReservations = [...reservations]; //create a copy of the original array, avoiding to modify it
+        sortedReservations.sort((a, b) => new Date(a.checkIn) - new Date(b.checkIn));//sort the reservations + transform strings into date value + use a compare function
+        return sortedReservations
+    }
+
+    function isAvailable(reservation) {//function that will be used in the function
+        const checkIn = reservation.checkIn;//create variables with the values of the properties of the reservation that wants to be made
+        const checkOut = reservation.checkOut;
+
+        for (const currentReservation of reservations) {//iterate through the array that contains all the reservations
+            const currentCheckIn = currentReservation.checkIn;
+            const currentCheckOut = currentReservation.checkOut;
             
-            if(index !== -1) {
-                reservations.splice(index, 1)
-            } else {
-                throw new Error("La reservación que se busca remover no existe");
-            };
+            if (//if both reservations overlap in the dates
+                (checkIn >= currentCheckIn && checkIn < currentCheckOut) ||
+                (checkOut > currentCheckIn && checkOut <= currentCheckOut) ||
+                (checkIn <= currentCheckIn && checkOut >= currentCheckOut)
+            ) {
+                if(currentReservation.roomNumber === reservation.roomNumber) {//but also in the room numbers
+                    return false; //that means that the room is not available por reservation
+                }
+            }
+        }
+        return true;//if after the iteration there´s no overlapong, it means that the room is available
+    }
 
+    function getAvailableRooms(checkIn, checkOut) {//return a list of available rooms for the checkin and checkout dates
+        const availableRooms = [];
 
-        },
+        for (let i = 1; i <= rooms; i++) {//iterate through all the rooms, starting in number room #1
+            const reservation = {checkIn, checkOut, roomNumber: i};//create an object with the values of the current iteration
 
-        getReservations() {
-            console.log(reservations);
-            return reservations;
-        },
+            if(isAvailable(reservation)) {//use the previously created function to verifiy if it´s available
+                availableRooms.push(i);//if it´s already avaliable, add to the array of available rooms
+            }
+        }
+        // console.log(availableRooms);
+        return availableRooms;//return what are the available rooms
+    };
 
-        // getAvailableRooms(checkIn, checkOut) {
-
-        // },
+    return {//this part of the script allows the function to activate the method that user wants to use
+        searchReservation,
+        getSortReservations,
+        addReservation,
+        removeReservation,
+        getReservations,
+        getAvailableRooms,
     };
 }
+
+
 
 
 
@@ -181,70 +227,37 @@ function hotelSystem(rooms) {
 
 const hotel = hotelSystem(10);
 
-// Agregar una nueva reservación
-// hotel.addReservation({
-//   id: 1,
-//   name: "John Doe",
-//   checkIn: "01/01",
-//   checkOut: "02/01",
-//   roomNumber: 1,
-// });
-
-// hotel.getReservations();
-
-
-
-
-// Input 2:
-
-// const hotel = hotelSystem(10);
-
 hotel.addReservation({
   id: 1,
   name: "John Doe",
-  checkIn: "04/01",
-  checkOut: "02/01",
+  checkIn: "02/01",
+  checkOut: "02/04",
   roomNumber: 1,
 });
 
 hotel.addReservation({
   id: 2,
   name: "Pepe Doe",
-  checkIn: "01/01",
-  checkOut: "02/01",
+  checkIn: "02/01",
+  checkOut: "02/07",
   roomNumber: 7,
 });
+
+hotel.addReservation({
+    id: 3,
+    name: "Alejo Cangrejo",
+    checkIn: "01/01",
+    checkOut: "02/01",
+    roomNumber: 3,
+  });
 
 // Buscar una resevación hecha
 hotel.searchReservation(2);
 
-// hotel.removeReservation(1);
+hotel.removeReservation(1);
 
 hotel.getReservations();
 hotel.getSortReservations();
 
-
-
-
-// Input 3:
-
-// const hotel = hotelSystem(10);
-
-// hotel.addReservation({
-//   id: 1,
-//   name: "John Doe",
-//   checkIn: "01/01",
-//   checkOut: "02/01",
-//   roomNumber: 1,
-// });
-
-// hotel.addReservation({
-//   id: 2,
-//   name: "Pepe Doe",
-//   checkIn: "01/01",
-//   checkOut: "10/01",
-//   roomNumber: 9,
-// });
-
 // // Buscamos habitaciones disponibles entre el 01 y el 05 del primer mes
-// hotel.getAvailableRooms("01/01", "05/01")
+hotel.getAvailableRooms("01/01", "05/01")
